ff583481dea8047c930ec0567a659ef6552ec2fa
Furkan <furser06@gmail.com>
2024-03-17 17:27:07 +0300

    working opengl window

--- ModernOpenGL/ModernOpenGL.cpp
+++ ModernOpenGL/ModernOpenGL.cpp
@@ -11,20 +11,68 @@ const GLint WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
-    //Initialise GLFW
-    if (!glfwInit()) {
-        printf("GLFW initialisation failed");
-        glfwTerminate();
-        return 1;
-    }
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		printf("GLFW initialisation failed!");
+		glfwTerminate();
+		return 1;
+	}
 
-#   // Setup GLFW window properties
-    // OpenGL version
+	// Setup GLFW window properties
+	// OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Core Profile = No Backwards Compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow Forward Compatbility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+	// Create the window
+	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
 
-    std::cout << "Hello World!\n";
+	// Get Buffer Size information
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set context for GLEW to use
+	glfwMakeContextCurrent(mainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW initialisation failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// Loop until window closed
+	while (!glfwWindowShouldClose(mainWindow))
+	{
+		// Get + Handle user input events
+		glfwPollEvents();
+
+		// Clear window
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
+	return 0;
 }
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
